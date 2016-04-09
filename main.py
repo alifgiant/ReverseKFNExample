c0d6bddf5f03fe60fe3ccb582e25a5144d400a30
maakbar <maakbar>
2016-04-09 19:14:01 +0700

    res pointer

---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib
+import res
+
+file_restaurant = open(res.file_restaurant, 'r')
+
+for line in file_restaurant:
+    print line
---
+++ res.py
@@ -0,0 +1,2 @@
+file_restaurant = 'input_restaurant.csv'
+file_customer = 'input_customer.csv'
