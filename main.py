e71869c6120a83e22925c901411c01e0cf974854
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2016-04-11 14:04:50 +0700

    visualisasi ok

--- main.py
+++ main.py
@@ -1,15 +1,75 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import res
 import spatial
+import random
+import heapq
 
 
 def get_point(split):
-    return split[0], spatial.Point(int(split[1]), int(split[2]))
+    return spatial.Point(int(split[1]), int(split[2]))
+
+
+def mark_circle(point, color='b', rad=0.3):
+    x, y = point.x, point.y
+    circle = plt.Circle((x, y), radius=rad, color=color)
+    fig.gca().add_artist(circle)
 
 file_restaurant = open(res.file_restaurant, 'r')
 restaurant_lines = [line[:-1] for line in file_restaurant]
-
 restaurant_position = [get_point(line.split(',')) for line in restaurant_lines]
 
-for i in restaurant_position:
-    print i[0], i[1]
+file_customer = open(res.file_customer, 'r')
+customer_lines = [line[:-1] for line in file_customer]
+customer_position = [get_point(line.split(',')) for line in customer_lines]
+
+fig = plt.figure()
+
+rand_index = random.randint(0, len(restaurant_position)-1)
+rand_index = 29
+query_point = restaurant_position[rand_index]
+
+mark_circle(query_point, color='r', rad=0.5)
+plt.text(query_point.x, query_point.y, 'query')
+
+
+for customer in customer_position:
+    mark_circle(customer)
+
+customer_list = list()
+for customer in customer_position:
+    ax = [spatial.get_distance(customer, restaurant) for restaurant in restaurant_position]
+    furthest = max(ax)
+    restaurant = ax.index(furthest)
+    # print rand_index, restaurant
+    if rand_index == restaurant:
+        customer_list.append(customer)
+    # kfn = heapq.nlargest(k, ax)
+    # kfn = [ax.index(val) for val in kfn]
+    # if query_point == kfn:
+    #     distance_list.append(kfn)
+
+k = 2
+ax = [spatial.get_distance(query_point, point) for point in customer_list]
+kfn = heapq.nlargest(k, ax)
+kfn = [customer_list[ax.index(dist)] for dist in kfn]
+
+x_point = [[query_point.x, pos.x] for pos in kfn]
+y_point = [[query_point.y, pos.y] for pos in kfn]
+
+for segment_x, segment_y in zip(x_point, y_point):
+    plt.plot(segment_x, segment_y, color='gold')
+
+dist = [spatial.get_distance(query_point, customer) for customer in customer_position]
+ax = heapq.nlargest(k, dist)
+kfn = [customer_position[dist.index(val)] for val in ax]
+
+x_point = [[query_point.x, pos.x] for pos in kfn]
+y_point = [[query_point.y, pos.y] for pos in kfn]
+
+for segment_x, segment_y in zip(x_point, y_point):
+    plt.plot(segment_x, segment_y, color='magenta')
+
+
+plt.axis([0, 100, 0, 100])
+# plt.axis([0, 20, 0, 15])
+plt.show()
